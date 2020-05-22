ab0e3a09f658b8af0deb22cd12853ecf626cd8db
Liam Wynn <wynnliam@pdx.edu>
2020-05-22 13:08:02 -0700

    Added procedure render_scene

--- src/scene.c
+++ src/scene.c
@@ -2,6 +2,8 @@
 
 #include "scene.h"
 
+#include <stdio.h>
+
 /*
     NOTES ABOUT Coordinate system.
 
@@ -23,3 +25,32 @@
 // Distance from the camera position
 // to the viewport in space.
 #define FOCAL_LENGTH    1
+
+// Renders scene to image file.
+// TODO: Split rendering and file writing logic
+void render_scene() {
+    const int image_width = 256;
+    const int image_height = 256;
+
+    fprintf(stderr, "Writing image file...\n");
+    printf("P3\n%d %d 255\n", image_width, image_height);
+
+    int row, col;
+    float r, g, b;
+    int ir, ig, ib;
+    for(row = image_height - 1; row >= 0; row--) {
+        fprintf(stderr, "Rows left: %d\n", row);
+        for(col = 0; col < image_width; col++) {
+            r = (float)col / (image_width - 1);
+            g = (float)row / (image_height - 1);
+            b = 0.25f;
+
+            ir = (int)(255 * r);
+            ig = (int)(255 * g);
+            ib = (int)(255 * b);
+
+            printf("%d %d %d\n", ir, ig, ib);
+        }
+    }
+    fprintf(stderr, "Done!\n");
+}
