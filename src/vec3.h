feebb99f3124a0fe23871b829cdbeb2e91074fe1
Liam Wynn <wynnliam@pdx.edu>
2020-05-22 11:07:50 -0700

    Added vec3_mul

--- src/vec3.h
+++ src/vec3.h
@@ -19,9 +19,13 @@ typedef struct {
 typedef vec3 color3;
 
 extern void vec3_negate(vec3* vec);
+
 extern void vec3_add(const vec3* a, const vec3* b, vec3* out);
 extern void vec3_sub(const vec3* a, const vec3* b, vec3* out);
+extern void vec3_mul(const vec3* a, const vec3* b, vec3* out);
+
 extern void vec3_scale(vec3* to_scale, const double scale_by);
+
 extern double vec3_length(const vec3* vec);
 extern double vec3_length_squared(const vec3* vec);
 
