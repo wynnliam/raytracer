5ad406ee548484ad424725292942f9c814a6667c
Liam Wynn <wynnliam@pdx.edu>
2020-05-25 11:48:45 -0700

    Added hit_record struct

--- src/ray.h
+++ src/ray.h
@@ -7,6 +7,16 @@ typedef struct {
     vec3 direction;
 } ray;
 
+typedef struct {
+    // The point on the object where the ray hit.
+    vec3 point;
+    // The normal of the surface where the ray hit.
+    vec3 normal;
+    // If you have the ray that hit the object, you can use
+    // it plus this to derive point.
+    double t;
+} hit_record;
+
 // Return the position of the ray at some moment in time.
 // The formula for this is origin + direction * t.
 extern void ray_at(const double t, const ray* ray, vec3* position);
