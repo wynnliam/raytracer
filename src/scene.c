51cd3a557f71459a496e4e567ec9c067b0a6dc6e
Liam Wynn <wynnliam@pdx.edu>
2020-05-26 16:26:57 -0700

    Updated a part of scene to use camera struct. Incomplete but have to step away now

--- src/scene.c
+++ src/scene.c
@@ -23,19 +23,7 @@
     which is centered at (0, 0, -1).
 */
 
-// Image width / image height
-#define ASPECT_RATIO 16.0 / 9.0
-
-#define VIEWPORT_H      2
-#define VIEWPORT_W      ASPECT_RATIO * VIEWPORT_H
-
-// Distance from the camera position
-// to the viewport in space.
-#define FOCAL_LENGTH    1
-
-static vec3 cam_pos;
-static vec3 vertical, horizontal;
-static vec3 viewport_lower_left_corner;
+static camera cam;
 
 // Elements in the scene
 static thing my_sphere;
@@ -78,22 +66,26 @@ static color3 ray_color(const ray* ray) {
 }
 
 void initialize_renderer() {
-    cam_pos._X = 0.0; cam_pos._Y = 0.0; cam_pos._Z = 0.0;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.viewport_h = 2.0;
+    cam.viewport_w = cam.aspect_ratio * cam.viewport_h;
+    cam.focal_length = 1.0;
 
-    vertical._X = 0.0; vertical._Y = VIEWPORT_H; vertical._Z = 0.0;
-    horizontal._X = VIEWPORT_W; horizontal._Y = 0.0; horizontal._Z = 0.0;
+    cam.origin._X = 0; cam.origin._Y = 0; cam.origin._Z = 0;
 
-    viewport_lower_left_corner = cam_pos;
-    vec3 to_sub = horizontal;
+    cam.vertical._X = 0; cam.vertical._Y = cam.viewport_h; cam.vertical._Z = 0.0;
+    cam.horizontal._X = cam.viewport_w; cam.horizontal._Y = 0.0; cam.horizontal._Z = 0.0;
+
+    cam.lower_left_corner = cam.origin;
+    vec3 to_sub = cam.horizontal;
+
     vec3_scale(&to_sub, 0.5);
-    vec3_sub(&viewport_lower_left_corner, &to_sub, &viewport_lower_left_corner);
-
-    to_sub = vertical;
+    vec3_sub(&cam.lower_left_corner, &to_sub, &cam.lower_left_corner);
+    to_sub = cam.vertical;
     vec3_scale(&to_sub, 0.5);
-    vec3_sub(&viewport_lower_left_corner, &to_sub, &viewport_lower_left_corner);
-
-    to_sub._X = 0.0; to_sub._Y = 0.0; to_sub._Z = FOCAL_LENGTH;
-    vec3_sub(&viewport_lower_left_corner, &to_sub, &viewport_lower_left_corner);
+    vec3_sub(&cam.lower_left_corner, &to_sub, &cam.lower_left_corner);
+    to_sub._X = 0.0; to_sub._Y = 0.0; to_sub._Z = cam.focal_length;
+    vec3_sub(&cam.lower_left_corner, &to_sub, &cam.lower_left_corner);
 
     // Initialize objects in scene
     my_sphere.data = (void*)&my_sphere_data;
@@ -115,7 +107,7 @@ void initialize_renderer() {
 // TODO: Split rendering and file writing logic
 void render_scene() {
     const int image_width = 384;
-    const int image_height = (int)(image_width / (ASPECT_RATIO));
+    const int image_height = (int)(image_width / (cam.aspect_ratio));
 
     fprintf(stderr, "Writing image file...\n");
     printf("P3\n%d %d 255\n", image_width, image_height);
@@ -130,12 +122,8 @@ void render_scene() {
     // They are a percentage of the image width and height
     // respectively.
     double u, v;
-    // What we add to lower_left_corner to get curr_ray's
-    // direction.
-    vec3 h_offset, v_offset;
 
-    // All rays begin at the camera position.
-    curr_ray.origin = cam_pos;
+    // TODO: Finish me!
 
     for(row = image_height - 1; row >= 0; row--) {
         fprintf(stderr, "Rows left: %d\n", row);
--- src/scene.h
+++ src/scene.h
@@ -1,4 +1,6 @@
 // Liam Wynn, 5/22/2020, Raytracer
 
+#include "camera.h"
+
 extern void initialize_renderer();
 extern void render_scene();
