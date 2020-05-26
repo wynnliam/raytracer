82c14489092b055f8a275a4e5023b5c64a0e9546
Liam Wynn <wynnliam@pdx.edu>
2020-05-26 13:04:16 -0700

    Implemented thing_list_hit

--- src/thing.c
+++ src/thing.c
@@ -13,5 +13,20 @@ int add_thing_to_list(thing* to_add, thing_list* add_to) {
 }
 
 int thing_list_hit(const ray* ray, const double t_min, const double t_max, void* data, hit_record* result) {
-    return 0;
+    thing_list* list = (thing_list*)data;
+    hit_record temp_record;
+    int hit_anything = 0;
+    double t_closest = t_max;
+    int i;
+
+    for(i = 0; i < list->len; i++) {
+        void* thing_data = list->things[i]->data;
+        if(list->things[i]->hit(ray, t_min, t_closest, thing_data, &temp_record)) {
+            hit_anything = 1;
+            t_closest = temp_record.t;
+            *result = temp_record;
+        }
+    }
+
+    return hit_anything;
 }
