26d3902363ec03214e209f4dcb27a96e292a357a
Liam Wynn <wynnliam@pdx.edu>
2020-06-02 12:33:16 -0700

    Added data member to material struct

--- src/material.h
+++ src/material.h
@@ -6,7 +6,10 @@
 #include "ray.h"
 
 typedef struct {
-    int (*scatter_check)(const ray* in, const hit_record* record, const color3* attenuation, ray* scattered);
+    // Use this to store whatever information you want
+    // when doing a computation.
+    void* data;
+    int (*scatter_check)(const ray* in, const hit_record* record, const color3* attenuation, void* data, ray* scattered);
 } material;
 
 #endif // MATERIAL
