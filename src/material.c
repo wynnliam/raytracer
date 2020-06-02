412e7d638b97514f5447998ecefc756041dcfacd
Liam Wynn <wynnliam@pdx.edu>
2020-06-02 12:41:49 -0700

    Implemented lambert_scatter

--- src/material.c
+++ src/material.c
@@ -1,1 +1,18 @@
 // Liam Wynn, 6/2/2020, Raytracer
+
+#include "material.h"
+
+int lambert_scatter(const ray* in, const hit_record* record, color3* attenuation, void* data, ray* scattered) {
+    vec3 scattered_dir;
+    vec3 rand_vec;
+
+    vec3_rand(&rand_vec);
+    vec3_add(&(record->normal), &rand_vec, &scattered_dir);
+
+    scattered->origin = record->point;
+    scattered->direction = scattered_dir;
+
+    *attenuation = ((lambert_data*)data)->albedo;
+
+    return 0;
+}
--- src/material.h
+++ src/material.h
@@ -9,7 +9,13 @@ typedef struct {
     // Use this to store whatever information you want
     // when doing a computation.
     void* data;
-    int (*scatter_check)(const ray* in, const hit_record* record, const color3* attenuation, void* data, ray* scattered);
+    int (*scatter_check)(const ray* in, const hit_record* record, color3* attenuation, void* data, ray* scattered);
 } material;
+
+typedef struct {
+    color3 albedo;
+} lambert_data;
+
+int lambert_scatter(const ray* in, const hit_record* record, color3* attenuation, void* data, ray* scattered);
 
 #endif // MATERIAL
